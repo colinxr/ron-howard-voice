ab298176015204bd3229fb92dff0dbf6943f2ab7
colinxr <colinxr@gmail.com>
2017-06-09 12:55:25 -0400

    hot fix

--- bot.js
+++ bot.js
@@ -10,25 +10,28 @@ const retweet = function(){
     lang: 'en'
     }
 
-    Twitter.get('search/tweets', params, function(err, data){
-      if (!err){
-        let retweetId = data.statuses[0].id_str;
-
-        Twitter.post('statuses/retweet/:id', {
-          id: retweetId
-        }, function(err, res){
-          if (res){
-            console.log('Retweeted!');
-          }
-          if (err){
-            console.log('Something went wrong while tweeting.');
-          }
-
-        });
-      }
-      else {
-        console.log('Something went wrong while searching.');
-      }
+    Twitter.get('search/tweets', params, function(err, data) {
+      // if there no errors
+        if (!err) {
+          // grab ID of tweet to retweet
+            var retweetId = data.statuses[0].id_str;
+            // Tell TWITTER to retweet
+            Twitter.post('statuses/retweet/:id', {
+                id: retweetId
+            }, function(err, response) {
+                if (response) {
+                    console.log('Retweeted.');
+                }
+                // if there was an error while tweeting
+                if (err) {
+                    console.log('Something went wrong while tweeting.');
+                }
+            });
+        }
+        // if unable to Search a tweet
+        else {
+          console.log('Something went wrong searching...');
+        }
     });
 }
 
