577947193acc325f0444a54b960cc55985267353
colinxr <colinxr@gmail.com>
2017-09-11 12:57:38 -0400

    es6 functions

--- bot.js
+++ bot.js
@@ -8,30 +8,28 @@ const Twitter = new twit({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-const retweet = function(){
+const retweet = () => {
   let params = {
     q: 'ron howard voice',
     result_type: 'recent',
     lang: 'en'
     }
 
-    Twitter.get('search/tweets', params, function(err, data) {
+    Twitter.get('search/tweets', params, (err, data) => {
       // if there no errors
         if (!err) {
           // grab ID of tweet to retweet
-            let retweetId = data.statuses[0].id_str;
-            // Tell TWITTER to retweet
-            Twitter.post('statuses/retweet/:id', {
-                id: retweetId
-            }, function(err, response) {
-                if (response) {
-                    console.log('Retweeted.');
-                }
-                // if there was an error while tweeting
-                if (err) {
-                    console.log('Something went wrong while tweeting.');
-                }
-            });
+          let retweetId = data.statuses[0].id_str;
+          // Tell TWITTER to retweet
+          Twitter.post('statuses/retweet/:id', {
+            id: retweetId
+          }, (err, response) => {
+            if (response)
+              console.log('Retweeted.');
+            // if there was an error while tweeting
+            if (err)
+              console.log('Something went wrong while tweeting.');
+          });
         }
         // if unable to Search a tweet
         else {
